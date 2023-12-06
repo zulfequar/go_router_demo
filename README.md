ab1189463258d4b551ccd16b6f113e56e9e47530
Zulfequar Ali <z.ali@sundewsolutions.com>
2023-12-06 15:25:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# GoRouter Demo
+
+A Flutter project that demonstrates the go_router Flutter package which is a declarative routing 
+package for Flutter that uses the Router API to provide a convenient, url-based API for navigating 
+between different screens. URL patterns, navigate using a URL, handle deep links, and a number of 
+other navigation-related scenarios can be defined.
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GoRouter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+        title: const Text('GoRouter Demo'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('GoRouter Demo'),
+          ],
+        ),
+      ),
+    );
+  }
+}
